91435b3826b31d76bb53b850da6f35857afb0c1c
AlfredGOD27 <alfred.david@thehoneycombers.com>
2018-03-12 16:18:45 +0800

    exclude school profiles from latest news -  HKA homepage

--- parts/home/latest-posts.php
+++ parts/home/latest-posts.php
@@ -5,7 +5,7 @@
         $args = array (
             'post_type'              => array( 'post' ),
             'post_status'            => array( 'publish' ),
-            'category__not_in'       => array( 23069, 30543, 30699, 30312, 30320, 30313, 30350, 30351, 30349, 30317, 30318, 30315, 30319 ),
+            'category__not_in'       => array( 23069, 30543, 30699, 30312, 30320, 30313, 30350, 30351, 30349, 30317, 30318, 30315, 30319, 30418 ),
             'posts_per_page'         => 6,
             'order'                  => 'DESC',
             'orderby'                => 'date',
@@ -24,7 +24,7 @@
 </div>
 
 <div class="row text-center button-container">
-    <div class="load-more" data-pages="<?php echo $query->max_num_pages; ?>" data-nextpage="2" data-ppp="6" data-hideSticky="1" data-excludeCat="23069,30543,30699,30312,30320,30313,30350,30351,30349,30317,30318,30315,30319">
+    <div class="load-more" data-pages="<?php echo $query->max_num_pages; ?>" data-nextpage="2" data-ppp="6" data-hideSticky="1" data-excludeCat="23069,30543,30699,30312,30320,30313,30350,30351,30349,30317,30318,30315,30319,30418">
         <span>LOAD MORE</span>
     </div>
 </div>
